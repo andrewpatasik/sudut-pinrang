f1be51e665fb6c5e7f63a926344ee8a3045c4ea2
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2023-06-08 14:01:07 +0800

    init react form hook

--- src/pages/daftar.tsx
+++ src/pages/daftar.tsx
@@ -1,6 +1,23 @@
 import Head from "next/head";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type SubmitPlaceInputs = {
+  name: string;
+  address: string;
+  rating: string;
+};
 
 const SubmitPlace = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubmitPlaceInputs>();
+
+  const onSubmit: SubmitHandler<SubmitPlaceInputs> = ({ name }) => {
+    console.log(name);
+  };
+
   return (
     <div>
       <Head>
@@ -9,7 +26,34 @@ const SubmitPlace = () => {
         </title>
       </Head>
       <section>
-        <h1>Daftar</h1>
+        <h1 className="text-2xl mb-4">Daftar Tempat Nongkrong</h1>
+        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col space-y-1 mb-2 w-2/3">
+            <label className="text-lg">Nama</label>
+            <input
+              className="px-2.5 py-2 border border-black"
+              type="text"
+              {...register("name", { required: true })}
+              placeholder="nama tempat/restoran/kedai..."
+            />
+          </div>
+
+          <div className="flex flex-col space-y-1 mb-2 w-2/3">
+            <label className="text-lg">Alamat</label>
+            <input
+              className="px-2.5 py-2 border border-black"
+              type="text"
+              {...register("address", {
+                required: true,
+              })}
+              placeholder="jalan jend sudirman..."
+            />
+          </div>
+
+          <div className="w-1/4 mt-4">
+            <button className="w-full bg-teal-500 py-2.5">Daftar</button>
+          </div>
+        </form>
       </section>
     </div>
   );

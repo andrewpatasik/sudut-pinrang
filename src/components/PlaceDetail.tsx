016081a62a709cb7a29e5a27e862b2221f807004
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2023-06-10 11:06:04 +0800

    fix fetching on dynamic page using server-side rendering

---
+++ src/components/PlaceDetail.tsx
@@ -0,0 +1,3 @@
+export default function PlaceDetail({ details }: any) {
+  return <div>{details.name}</div>;
+}
--- src/pages/places/place/[id].tsx
+++ src/pages/places/place/[id].tsx
@@ -1,28 +1,14 @@
-import { useRouter } from "next/router";
+import PlaceDetail from "@/components/PlaceDetail";
 
 export default function Place ({ placeDetail }: any) {
-  const { name } = placeDetail
   return (
     <div>
-      <h1>{name}</h1>
+      <PlaceDetail details={placeDetail} />
     </div>
   );
 };
 
-export const getStaticPaths = async () => {
-  return {
-    paths: [
-      {
-        params: {
-          id: '1',
-        },
-      }, // See the "paths" section below
-    ],
-    fallback: false, // false or "blocking"
-  }
-}
-
-export async function getStaticProps({ params }: any) {
+export async function getServerSideProps({ params }: any) {
   const { id } = params;
   const res = await fetch(`http://localhost:3000/api/places/${id}`);
   const placeDetail = await res.json();
